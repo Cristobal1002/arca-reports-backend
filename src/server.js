8cf5c3f4eb4457b9ccec384f17a271d0015da862
Cristobal1002 <cristobal1002@gmail.com>
2024-03-22 16:51:27 -0500

    Vercel config

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 import express  from 'express';
 import {routes} from "./routes/main.routes.js";
+import cors from 'cors'
+
 
 export default async () => {
     const port = process.env.SERVER_PORT;
     const app = express();
     app.use(express.json({limit: '20mb'}));
     app.use(express.urlencoded({extended: true, limit: '20mb'}));
+
+    app.use(cors());
 
     routes(app)
     app.listen(port, () => {
