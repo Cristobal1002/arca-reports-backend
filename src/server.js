d3f2f5e225495713b4dea600d31e6c1f48500b50
Cristobal1002 <cristobal1002@gmail.com>
2024-03-22 22:53:30 -0500

    fix dotenv

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express  from 'express';
 import {routes} from "./routes/main.routes.js";
 import cors from 'cors'
-
+require('dotenv').config();
 
 export default async () => {
     const port = process.env.SERVER_PORT;
